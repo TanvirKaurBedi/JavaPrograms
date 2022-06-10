77352979ce83d1c389b6504d4c1a21aa1a0d2a0d
Tanvir <tanvirkaurbedi@gmail.com>
2022-06-10 08:22:59 -0700

    reverse a string

---
+++ reverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverseString {
+    public static void main(String[] args) {
+        String word;
+        int i, k;
+        String reversedWord = " ";
+        System.out.println("Plz enter a word to be reversed");
+        Scanner sc = new Scanner(System.in);
+        word = sc.nextLine();
+        k = word.length();
+        for (i = k - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+        System.out.println(reversedWord);
+    }
+}
