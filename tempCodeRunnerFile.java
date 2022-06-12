9a27a07041636f2c04064634549ae69e7eb7b3a6
Tanvir <tanvirkaurbedi@gmail.com>
2022-06-12 06:17:37 -0700

    pattern

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,6 @@
-       System.out.println(sum);
+for (j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 5; j >= i; j--) {
+                System.out.print("* ");
+            }
