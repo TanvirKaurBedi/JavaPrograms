80c39ab18bcaf29f0fac001d19f122fc5f7bd983
Tanvir <tanvirkaurbedi@gmail.com>
2022-06-12 06:55:36 -0700

    sand glass

--- tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
- // for (i = 1; i <= 4; i++) {
-        //     for (j = 1; j <= i; j++) {
-        //         System.out.print(" ");
-        //     }
-        //     for (j = 5; j >= i; j--) {
-        //         System.out.print("* ");
-        //     }
-        //     System.out.println();
-        // }
